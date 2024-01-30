247ba61d006fa1896f2186c75508e480d1cfc154
Yujin14 <106482110+Yujin14@users.noreply.github.com>
2024-01-30 09:09:52 +0800

    Update: Nav

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// index.js or your entry file
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.render(
+  <Router>
     <App />
-  </React.StrictMode>,
-)
+  </Router>,
+  document.getElementById('root')
+);
