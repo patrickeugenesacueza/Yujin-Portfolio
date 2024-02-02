16b3783f4b5d4dedf1ec98652e9180dd78cc473e
Yujin14 <106482110+Yujin14@users.noreply.github.com>
2024-02-02 09:45:55 +0800

    Update: HeroSection.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Nav from './Pages/Nav'
+import HeroSection from './Pages/HeroSection'
 
 
 const App = () => {
   return(
     <div className='bg-slate-900'>
         <Nav/>
+        <HeroSection/>
     </div>
   )
 }
---
+++ portfolio/src/Pages/HeroSection.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { TypeAnimation } from "react-type-animation";
+
+const HeroSection = () => {
+  return (
+    <section className="w-full h-[80vh] bg-[#fff] pt-[5em]">
+      <div className="text-center">
+        <TypeAnimation
+          sequence={[
+            "Front-end Developer",
+            1000,
+            "Mobile Application Developer",
+            1000,
+            "Desktop Support Engineer",
+            1000,
+          ]}
+          wrapper="span"
+          speed={50}
+          className="text-[#333] text-[1.8em] md:text-[calc(1em + 2.4vw)] lg:text-3em font-[900] leading-[1.2]"
+          repeat={Infinity}
+        />
+      </div>
+      {/* Card-box */}
+      <div>
+
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
