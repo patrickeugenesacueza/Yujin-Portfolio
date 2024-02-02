caa7ee8ef331d6e9edb1ef5589577309fa42f8b8
Yujin14 <106482110+Yujin14@users.noreply.github.com>
2024-02-02 18:22:13 +0800

    Update HeroSection.jsx

--- portfolio/src/Pages/HeroSection.jsx
+++ portfolio/src/Pages/HeroSection.jsx
@@ -4,7 +4,7 @@ import { TypeAnimation } from "react-type-animation";
 const HeroSection = () => {
   return (
     <section className="w-full h-[80vh] bg-[#fff] pt-[5em]">
-      <div className="text-center">
+      <div className="text-center block">
         <TypeAnimation
           sequence={[
             "Front-end Developer",
@@ -16,14 +16,13 @@ const HeroSection = () => {
           ]}
           wrapper="span"
           speed={50}
-          className="text-[#333] font-[Lato] text-[1.8em] md:text-[calc(1em + 2.4vw)] lg:text-3em font-[900] leading-[1.2]"
+          className="text-[#333] font-[Lato] text-[1.2em] md:text-[3em] lg:text-3em font-[900] leading-[1.2]"
           repeat={Infinity}
         />
-        <div>
-            <p>Hey there! I'm Eugene, a passionate developer ready to dive into the world of challenges with boundless energy. No limits, just pure enthusiasm to create amazing things together</p>
+        <div className="">
+            <p>Hey there! I'm Eugene, a passionate person ready to dive into the world of challenges with boundless energy. No limits, just pure enthusiasm to create amazing things together</p>
         </div>
       </div>
-      {/* Card-box */}
       <div></div>
     </section>
   );
