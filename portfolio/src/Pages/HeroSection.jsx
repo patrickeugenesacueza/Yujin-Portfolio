fbc27034f82c156764492bd82ee44cbe707a0eb0
Yujin14 <106482110+Yujin14@users.noreply.github.com>
2024-02-08 14:01:35 +0800

    Update: HeroSection

--- portfolio/src/Pages/HeroSection.jsx
+++ portfolio/src/Pages/HeroSection.jsx
@@ -5,6 +5,8 @@ import { TypeAnimation } from "react-type-animation";
 import FrontEndLogo from "../assets/frontEndSVG.svg";
 import MobileLogo from "../assets/mobileDevSVG.svg";
 import TechSuppLogo from "../assets/techSupportSVG.svg";
+import ClrsDekstopSize from "../assets/clrsDekstopSize.png";
+import ClrsMobileSize from "../assets/clrsMobileSize.png";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
@@ -40,7 +42,7 @@ const HeroSection = () => {
       {/* grid box */}
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4 mt-10 px-10 sm:px-10 md:px-10 lg:px-[10em] xl:px-[10em]">
         {/* 1st container */}
-        <div className="bg-[#f9f9f9] py-[2em] sm:py-[8em] md:py-[10em] lg:py-[10em] xl:py-[2em] flex items-center justify-center text-center">
+        <div className="bg-[#F3F3F3] py-[2em] sm:py-[8em] md:py-[10em] lg:py-[10em] xl:py-[2em] flex items-center justify-center text-center">
           <div className="flex text-left md:block md:text-center lg:block lg:text-center xl:block xl:text-center 2xl:block 2xl:text-center">
             <img
               src={FrontEndLogo}
@@ -59,7 +61,7 @@ const HeroSection = () => {
         </div>
 
         {/* 2nd container */}
-        <div className="bg-[#f9f9f9] py-[2em] sm:py-[8em] md:py-[10em] lg:py-[10em] xl:py-[2em] flex items-center justify-center text-center">
+        <div className="bg-[#F3F3F3] py-[2em] sm:py-[8em] md:py-[10em] lg:py-[10em] xl:py-[2em] flex items-center justify-center text-center">
           <div className="flex text-left md:block md:text-center lg:block lg:text-center xl:block xl:text-center 2xl:block 2xl:text-center">
             <img src={MobileLogo} height={80} width={190} className="mx-auto" />
             <div className="mt-5 px-[2em]">
@@ -75,7 +77,7 @@ const HeroSection = () => {
         </div>
 
         {/* 3rd container */}
-        <div className="bg-[#f9f9f9] py-[2em] sm:py-[8em] md:py-[10em] lg:py-[10em] xl:py-[2em] flex items-center justify-center text-center">
+        <div className="bg-[#F3F3F3] py-[2em] sm:py-[8em] md:py-[10em] lg:py-[10em] xl:py-[2em] flex items-center justify-center text-center">
           <div className="flex text-left md:block md:text-center lg:block lg:text-center xl:block xl:text-center 2xl:block 2xl:text-center">
             <img
               src={TechSuppLogo}
@@ -100,7 +102,7 @@ const HeroSection = () => {
         </h2>
         <div className="h-[60vh] py-[1em] px-[1em] flex mx-[2em] md:mx-[4em] lg:mx-[5em] 2xl:mx-[5em]">
           <div className="w-1/4 h-full mr-[1.5em]">
-            <div className="w-full bg-[#e5e7eb] h-1/3"></div>
+            <div className="w-full bg-[#F3F3F3] h-1/3"></div>
             <div>
             <div className="flex items-center">
               {/* Updated to use the imported icon */}
@@ -111,12 +113,19 @@ const HeroSection = () => {
             </div>
               <span className="text-[1.5rem] text-[#333] font-serif">
                 I spearheaded the development of Clarise's portfolio utilizing
-                REACT JS, Tailwind CSS, and EmailJS, alongside various other
+                React JS, Tailwind CSS, and EmailJS, alongside various other
                 React packages
               </span>
             </div>
           </div>
-          <div className="w-3/4 h-full bg-[#e5e7eb]"></div>
+          <div className="w-3/4 h-full bg-[#F3F3F3] flex p-[1.5em]">
+            <div className="h-full w-2/3 bg-red-700 mr-[2em]">
+              <img src={ClrsDekstopSize} className="h-full w-full"/>
+            </div>
+            <div className="h-full w-1/3 bg-red-200">
+              <img src={ClrsMobileSize} className="h-full w-full"/>
+            </div>
+          </div>
         </div>
       </div>
     </section>
