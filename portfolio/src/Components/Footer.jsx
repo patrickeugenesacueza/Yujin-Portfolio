9ab699ea2ff342626b6000729c41618a41631f93
Yujin14 <106482110+Yujin14@users.noreply.github.com>
2024-02-14 03:25:52 +0800

    Update: Link.jsx

--- portfolio/src/Components/Footer.jsx
+++ portfolio/src/Components/Footer.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import TestimonialClarise from "../assets/testimonialClarise.jpg";
-
-import NavDetails from "../data/NavDetails.json";
 import SocialMediaDetails from "../data/SocialMedia.json";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import {
@@ -11,12 +8,13 @@ import {
   faSquareGithub,
   faLinkedinIn,
 } from "@fortawesome/free-brands-svg-icons";
+import Link from "./Link.jsx";
 
 export default function Footer() {
   return (
     <>
       <div className="w-full h-[80vh] md:h-[60vh] bg-[#282424] md:flex lg:flex xl:flex 2xl:flex mt-[2em] p-[2em] text-[#fff]">
-        <div className="  h-1/2 md:h-full lg:h-full xl:h-full 2xl:h-full w-full md:w-1/2 lg:w-1/2 xl:w-1/2 2xl:w-1/2">
+        <div className="h-1/2 md:h-full lg:h-full xl:h-full 2xl:h-full w-full md:w-1/2 lg:w-1/2 xl:w-1/2 2xl:w-1/2">
           <div className="flex">
             <div className="w-[22h] h-full mr-3">
               <img src={TestimonialClarise} className="w-[20vh] h-[20vh]" />
@@ -36,36 +34,44 @@ export default function Footer() {
               <p className="font-bold text-[1em]">
                 AVAILABLE APRIL 2024. HIRE NOW
               </p>
-              <FontAwesomeIcon icon={faChevronRight} className="ml-2 " />
+              <FontAwesomeIcon icon={faChevronRight} className="ml-2" />
             </div>
           </div>
         </div>
-        <div className=" h-[40vh] md:h-full lg:h-full xl:h-full 2xl:h-full w-full md:w-1/4 lg:w-1/4 xl:w-1/4 2xl:w-1/4 text-left md:text-right md:px-[2em]">
+        <div className="h-[40vh] md:h-full lg:h-full xl:h-full 2xl:h-full w-full md:w-1/4 lg:w-1/4 xl:w-1/4 2xl:w-1/4 text-left md:text-right md:px-[2em]">
           {SocialMediaDetails.map((item, index) => (
-            <div className="">
-              <div className="flex justify-start md:justify-end">
-                <FontAwesomeIcon
-                  icon={
-                    item.media === "Facebook"
-                      ? faSquareFacebook
-                      : item.media === "GitHub"
-                      ? faSquareGithub
-                      : faLinkedinIn
-                  }
-                  alt={`image ${index}`}
-                  className="h-[3vh] w-[3vh] mr-[1em] mt-1 "
-                />
-                <h4 className="uppercase text-xl font-bold ">{item.media}</h4>
-              </div>
-
+            <div key={index} className="">
+              <Link
+                url={
+                  item.media === "Facebook"
+                    ? "https://www.facebook.com/patrickeugene.sacueza"
+                    : item.media === "GitHub"
+                    ? "https://github.com/patrickeugenesacueza"
+                    : "https://www.linkedin.com/in/patrick-eugene-sacueza-18633826a/"
+                }
+              >
+                <div className="flex justify-start md:justify-end">
+                  <FontAwesomeIcon
+                    icon={
+                      item.media === "Facebook"
+                        ? faSquareFacebook
+                        : item.media === "GitHub"
+                        ? faSquareGithub
+                        : faLinkedinIn
+                    }
+                    alt={`image ${index}`}
+                    className="h-[3vh] w-[3vh] mr-[1em] mt-1 "
+                  />
+                  <h4 className="uppercase text-xl font-bold ">{item.media}</h4>
+                </div>
+              </Link>
               <p className="text-md leading-7 my-3 font-light">
                 {item.description}
               </p>
             </div>
           ))}
         </div>
-
-        <div className=" bg-red-200 h-full w-1/4 hidden md:block"></div>
+        <div className="bg-red-200 h-full w-1/4 hidden md:block"></div>
       </div>
     </>
   );
