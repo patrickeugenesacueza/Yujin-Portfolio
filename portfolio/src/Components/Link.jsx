c7fc1f8bccc4275ec6abf4768f9a1e67e21b4337
Yujin14 <106482110+Yujin14@users.noreply.github.com>
2024-02-14 02:47:47 +0800

    Create Link.jsx

---
+++ portfolio/src/Components/Link.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const Link = ({ url }) => {
+
+  return (
+    <div className="mt-3 relative">
+      <a
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="relative block"
+        style={{ overflow: "" }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <IonIcon name={icon} size={iconSize} style={iconStyle} />
+      </a>
+    </div>
+  );
+};
+
+Link.propTypes = {
+  url: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  iconSize: PropTypes.string.isRequired,
+  iconColor: PropTypes.string, // Add iconColor prop
+  hoverColor: PropTypes.string, // Add hoverColor prop
+};
+
+Link.defaultProps = {
+  iconSize: "medium",
+  iconColor: "#9b6c9a", // Set the custom color if not provided
+  hoverColor: "#b37eb5", // Set the hover color if not provided
+};
+
+export default Link;
