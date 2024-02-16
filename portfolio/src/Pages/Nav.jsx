37a67c5764cbcc76c882b6f5e94ed50c80256df2
Yujin14 <106482110+Yujin14@users.noreply.github.com>
2024-02-16 19:40:59 +0800

    Update: App.jsx

--- portfolio/src/Pages/Nav.jsx
+++ portfolio/src/Pages/Nav.jsx
@@ -7,58 +7,34 @@ import Logo from "../assets/Logo.svg";
 const Nav = () => {
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
-  const content = (
-    <>
-      <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-slate-900">
-        <ul className="text-center text-xl p-20">
-          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">
-            <Link to="Home">Home</Link>
-          </li>
-          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">
-            <Link to="About">About</Link>
-          </li>
-          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">
-            <Link to="Service">Service</Link>
-          </li>
-          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">
-            <Link to="Projects">Projects</Link>
-          </li>
-          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">
-            <Link to="Contacts">Contacts</Link>
-          </li>
-        </ul>
-      </div>
-    </>
-  );
 
   return (
     <nav>
       <div className="h-10vh flex justify-between z-50 text-white lg:py-5 px-20 py-4">
         <div className="flex items-center flex-1">
-          <img src={Logo}/>
+          <img src={Logo} alt="Logo" />
         </div>
         <div className="lg:flex md:flex lg:flex-1 items-center justify-end font-normal hidden">
           <div className="flex-10">
             <ul className="flex gap-8 mr-16 text-[18px]">
               <li className="relative after:bg-white after:absolute after:h-1 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer">
-                <Link to="Home">Home</Link>
+                <Link to="/">Home</Link>
               </li>
               <li className="relative after:bg-white after:absolute after:h-1 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer">
-                <Link to="About">About</Link>
+                <Link to="/About">About</Link>
               </li>
               <li className="relative after:bg-white after:absolute after:h-1 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer">
-                <Link to="Service">Service</Link>
+                <Link to="/Service">Service</Link>
               </li>
               <li className="relative after:bg-white after:absolute after:h-1 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer">
-                <Link to="Projects">Projects</Link>
+                <Link to="/Projects">Projects</Link>
               </li>
               <li className="relative after:bg-white after:absolute after:h-1 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer">
-                <Link to="Contacts">Contacts</Link>
+                <Link to="/Contacts">Contacts</Link>
               </li>
             </ul>
           </div>
         </div>
-        <div>{click && content}</div>
         <button className="block sm:hidden transition" onClick={handleClick}>
           {click ? <FaTimes /> : <CiMenuFries />}
         </button>
