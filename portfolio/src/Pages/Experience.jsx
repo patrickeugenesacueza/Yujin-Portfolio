d1c8ffb9503b9b0b1eddb619757b6a8fcc75ec36
Yujin14 <106482110+Yujin14@users.noreply.github.com>
2024-02-17 22:09:31 +0800

    Update: Nav.jsx

--- portfolio/src/Pages/Experience.jsx
+++ portfolio/src/Pages/Experience.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Experience() {
   return (
     <>
-      <div className="w-full h-[100vh] bg-red-500">
-        ABOUT
+      <div className="w-full h-[100vh] bg-blue-500">
+        EXPERIENCE
       </div>
     </>
   );
