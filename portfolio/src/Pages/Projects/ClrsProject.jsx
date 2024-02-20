7394d383b90d4a653f27cd47a8e7717f017e4c42
Yujin14 <106482110+Yujin14@users.noreply.github.com>
2024-02-21 01:46:15 +0800

    Update: ClrsProject.jsx
    Insert Footer component

--- portfolio/src/Pages/Projects/ClrsProject.jsx
+++ portfolio/src/Pages/Projects/ClrsProject.jsx
@@ -1,14 +1,14 @@
 import React from "react";
+import Footer from "../../Components/Footer";
 
-export default function ClrsProject(){
-    return(
-        <section className="w-full h-[100vh] flex">
-            <div className="w-1/4 h-full bg-red-700">
-
-            </div>
-            <div className="w-3/4 h-full bg-violet-800">
-
-            </div>
-        </section>
-    )
+export default function ClrsProject() {
+  return (
+    <section className="">
+      <div className="w-full h-[100vh] flex">
+        <div className="w-1/4 h-full bg-red-700"></div>
+        <div className="w-3/4 h-full bg-violet-800"></div>
+      </div>
+      <Footer />
+    </section>
+  );
 }
