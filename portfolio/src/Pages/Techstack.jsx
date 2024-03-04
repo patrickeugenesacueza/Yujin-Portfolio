4dd7bb6986fc02eb997596c6ed38dd258a0454d2
Yujin14 <106482110+Yujin14@users.noreply.github.com>
2024-03-05 03:52:37 +0800

    Update: Techstack.jsx

--- portfolio/src/Pages/Techstack.jsx
+++ portfolio/src/Pages/Techstack.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Footer from "../Components/Footer";
 
 export default function Techstack(){
     return(
         <>
             <section className="w-full h-auto bg-red-300">
-                TECHSTACK
+                
+            <Footer/>
             </section>
         </>
     )
